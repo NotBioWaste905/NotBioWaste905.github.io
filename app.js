566d083344cdfdb0388c7ef5b45a91ed5e57771b
NotBioWaste905 <59259188+NotBioWaste905@users.noreply.github.com>
2023-04-07 20:43:39 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,30 +4,57 @@ tg.expand();
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#2acd34";
 
-let item = "";
+let item = [];
 
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
 let btn3 = document.getElementById("btn3");
 let btn4 = document.getElementById("btn4");
 
-btn1.addEventListener("click", function(){
-    if (tg.MainButton.isVisible){
-        tg.MainButton.hide();
-    }
-    else{
-        tg.MainButton.setText("здаров молодой");
-        item = "1";
-        tg.MainButton.show();
-    }
+btn1.addEventListener("click", function () {
+  if (tg.MainButton.isVisible) {
+    tg.MainButton.hide();
+  } else {
+    tg.MainButton.setText(item.toString());
+    item.push("coffe1");
+    tg.MainButton.show();
+  }
+});
+btn2.addEventListener("click", function () {
+  if (tg.MainButton.isVisible) {
+    tg.MainButton.hide();
+  } else {
+    tg.MainButton.setText(item.toString());
+    item.push("coffe2");
+    tg.MainButton.show();
+  }
+});
+btn3.addEventListener("click", function () {
+  if (tg.MainButton.isVisible) {
+    tg.MainButton.hide();
+  } else {
+    tg.MainButton.setText(item.toString());
+    item.push("coffe3");
+    tg.MainButton.show();
+  }
+});
+btn4.addEventListener("click", function () {
+  if (tg.MainButton.isVisible) {
+    tg.MainButton.hide();
+  } else {
+    tg.MainButton.setText(item.toString());
+    item.push("coffe3");
+    tg.MainButton.show();
+  }
 });
 
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-    tg.sendData(item);
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
+  tg.sendData(item);
 });
 
 let usercard = document.getElementById("usercard");
 let p = document.createElement("p");
-p.innerText = `${tg.initDataUnsafe.user.first_name}`
+// p.innerText = `${tg.initDataUnsafe.first_name}`;
+p.innerText = `${item.toString()}`;
 
-usercard.appendChild(p)
+usercard.appendChild(p);
